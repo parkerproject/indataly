706f378a1c8b782307ee7356328e44536ab22e05
Parker <parkerproject@gmail.com>
2016-04-16 15:05:54 -0400

    updated

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var Hapi = require('hapi')
-var Inert = require('inert')
-var Vision = require('vision')
-var path = require('path')
-var Hapi_auth = require('hapi-auth-cookie')
-var HapiSwagger = require('hapi-swagger')
-var swaggerOptions = {
-  apiVersion: '1.0.0'
-}
-var server = new Hapi.Server()
+const Hapi = require('hapi')
+const Inert = require('inert')
+const Vision = require('vision')
+const path = require('path')
+// var Hapi_auth = require('hapi-auth-cookie')
+// var HapiSwagger = require('hapi-swagger')
+// var swaggerOptions = {
+//   apiVersion: '1.0.0'
+// }
+const server = new Hapi.Server()
 server.connection({
-    host: '0.0.0.0',
-    port: 3000
-  })
-  // Require the routes and pass the server object.
-var routes = require('./server/config/routes')(server)
+  host: '0.0.0.0',
+  port: 3000
+})
+// Require the routes and pass the server object.
+const routes = require('./server/config/routes')(server)
 
 global.appRoot = path.resolve(__dirname)
 
@@ -23,10 +23,8 @@ module.exports = server
 // Bootstrap Hapi Server Plugins, passes the server object to the plugins
 // require('./server/config/plugins')(server)
 
-server.register([Inert, Vision, Hapi_auth, {
-  register: HapiSwagger,
-  options: swaggerOptions
-}], function (err) {
+server.register([Inert, Vision], (err) => {
+  if (err)console.log(err)
   server.views({
     path: './server/views',
     engines: {
@@ -34,15 +32,15 @@ server.register([Inert, Vision, Hapi_auth, {
     }
   })
 
-  server.auth.strategy('session', 'cookie', {
-    password: 'dancingtomorrow',
-    cookie: 'sid-indataly',
-    redirectTo: '/login',
-    isSecure: false,
-    ttl: 15 * 60 * 60 * 1000
-  })
+  // server.auth.strategy('session', 'cookie', {
+  //   password: 'dancingtomorrow',
+  //   cookie: 'sid-indataly',
+  //   redirectTo: '/login',
+  //   isSecure: false,
+  //   ttl: 15 * 60 * 60 * 1000
+  // })
   server.route(routes)
-  server.start(function () {
+  server.start(() => {
     console.log('Server started at: ' + server.info.uri)
   })
 })
